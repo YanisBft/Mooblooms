e50360fbafc0441c74e7156294d5f953a6f7261e
Yanis48 <doublecraft.official@gmail.com>
2019-11-26 19:14:46 +0100

    updated to 1.15-pre2 & fixed deprecation

--- src/main/java/com/yanis48/mooblooms/entity/MoobloomFlowerFeatureRenderer.java
+++ src/main/java/com/yanis48/mooblooms/entity/MoobloomFlowerFeatureRenderer.java
@@ -4,14 +4,14 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.LayeredVertexConsumerStorage;
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.BlockRenderManager;
 import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRenderer;
 import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.render.entity.model.CowEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.util.math.Vector3f;
-import net.minecraft.util.math.MatrixStack;
 
 @Environment(EnvType.CLIENT)
 public class MoobloomFlowerFeatureRenderer<T extends MoobloomEntity> extends FeatureRenderer<T, CowEntityModel<T>> {
@@ -21,33 +21,34 @@ public class MoobloomFlowerFeatureRenderer<T extends MoobloomEntity> extends Fea
 	}
 
 	@Override
-	public void render(MatrixStack matrix, LayeredVertexConsumerStorage consumerStorage, int int_1, MoobloomEntity moobloom, float float_1, float float_2, float float_3, float float_4, float float_5, float float_6, float float_7) {
+	public void render(MatrixStack matrix, VertexConsumerProvider vertexConsumerProvider, int i, MoobloomEntity moobloom, float f, float g, float h, float j, float k, float l) {
 		if (!moobloom.isBaby() && !moobloom.isInvisible()) {
 			BlockRenderManager manager = MinecraftClient.getInstance().getBlockRenderManager();
 			BlockState state = moobloom.getFlowerState();
+			int m = LivingEntityRenderer.method_23622(moobloom, 0.0F);
 			matrix.push();
+			matrix.translate(0.20000000298023224D, -0.3499999940395355D, 0.5D);
+			matrix.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(-48.0F));
 			matrix.scale(-1.0F, -1.0F, 1.0F);
-			matrix.translate(-0.20000000298023224D, 0.3499999940395355D, 0.5D);
-			matrix.multiply(Vector3f.POSITIVE_Y.getRotationQuaternion(-42.0F));
-			int int_2 = LivingEntityRenderer.method_23622(moobloom, 0.0F);
-			matrix.push();
-			matrix.translate(-0.5D, -0.5D, 0.5D);
-			manager.renderDynamic(state, matrix, consumerStorage, int_1, int_2);
+			matrix.translate(-0.5D, -0.5D, -0.5D);
+			manager.renderBlockAsEntity(state, matrix, vertexConsumerProvider, i, m);
 			matrix.pop();
 			matrix.push();
-			matrix.translate(-0.10000000149011612D, 0.0D, -0.6000000238418579D);
-			matrix.multiply(Vector3f.POSITIVE_Y.getRotationQuaternion(-42.0F));
-			matrix.translate(-0.5D, -0.5D, 0.5D);
-			manager.renderDynamic(state, matrix, consumerStorage, int_1, int_2);
-			matrix.pop();
-			matrix.pop();
-			matrix.push();
-			this.getModel().getHead().rotate(matrix, 0.0625F);
+			matrix.translate(0.20000000298023224D, -0.3499999940395355D, 0.5D);
+			matrix.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(42.0F));
+			matrix.translate(0.10000000149011612D, 0.0D, -0.6000000238418579D);
+			matrix.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(-48.0F));
 			matrix.scale(-1.0F, -1.0F, 1.0F);
-			matrix.translate(0.0D, 0.699999988079071D, -0.20000000298023224D);
-			matrix.multiply(Vector3f.POSITIVE_Y.getRotationQuaternion(-12.0F));
-			matrix.translate(-0.5D, -0.5D, 0.5D);
-			manager.renderDynamic(state, matrix, consumerStorage, int_1, int_2);
+			matrix.translate(-0.5D, -0.5D, -0.5D);
+			manager.renderBlockAsEntity(state, matrix, vertexConsumerProvider, i, m);
+			matrix.pop();
+			matrix.push();
+			this.getModel().getHead().rotate(matrix);
+			matrix.translate(0.0D, -0.699999988079071D, -0.20000000298023224D);
+			matrix.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(-78.0F));
+			matrix.scale(-1.0F, -1.0F, 1.0F);
+			matrix.translate(-0.5D, -0.5D, -0.5D);
+			manager.renderBlockAsEntity(state, matrix, vertexConsumerProvider, i, m);
 			matrix.pop();
 		}
 	}
--- src/main/java/com/yanis48/mooblooms/init/MoobloomsEntities.java
+++ src/main/java/com/yanis48/mooblooms/init/MoobloomsEntities.java
@@ -6,21 +6,21 @@ import com.yanis48.mooblooms.entity.MoobloomEntity;
 import net.fabricmc.fabric.api.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCategory;
+import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-@SuppressWarnings("deprecation")
 public class MoobloomsEntities {
 	
-	public static final EntityType<MoobloomEntity> DANDELION_MOOBLOOM = register("dandelion_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(0.9F, 1.4F).build());
-	public static final EntityType<MoobloomEntity> POPPY_MOOBLOOM = register("poppy_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(0.9F, 1.4F).build());
-	public static final EntityType<MoobloomEntity> BLUE_ORCHID_MOOBLOOM = register("blue_orchid_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(0.9F, 1.4F).build());
-	public static final EntityType<MoobloomEntity> ALLIUM_MOOBLOOM = register("allium_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(0.9F, 1.4F).build());
-	public static final EntityType<MoobloomEntity> OXEYE_DAISY_MOOBLOOM = register("oxeye_daisy_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(0.9F, 1.4F).build());
-	public static final EntityType<MoobloomEntity> CORNFLOWER_MOOBLOOM = register("cornflower_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(0.9F, 1.4F).build());
-	public static final EntityType<MoobloomEntity> WITHER_ROSE_MOOBLOOM = register("wither_rose_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(0.9F, 1.4F).setImmuneToFire().build());
-	public static final EntityType<MoobloomEntity> BAMBMOO = register("bambmoo", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(0.9F, 1.4F).build());
+	public static final EntityType<MoobloomEntity> DANDELION_MOOBLOOM = register("dandelion_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
+	public static final EntityType<MoobloomEntity> POPPY_MOOBLOOM = register("poppy_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
+	public static final EntityType<MoobloomEntity> BLUE_ORCHID_MOOBLOOM = register("blue_orchid_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
+	public static final EntityType<MoobloomEntity> ALLIUM_MOOBLOOM = register("allium_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
+	public static final EntityType<MoobloomEntity> OXEYE_DAISY_MOOBLOOM = register("oxeye_daisy_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
+	public static final EntityType<MoobloomEntity> CORNFLOWER_MOOBLOOM = register("cornflower_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
+	public static final EntityType<MoobloomEntity> WITHER_ROSE_MOOBLOOM = register("wither_rose_moobloom", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).setImmuneToFire().build());
+	public static final EntityType<MoobloomEntity> BAMBMOO = register("bambmoo", FabricEntityTypeBuilder.create(EntityCategory.CREATURE, MoobloomEntity::new).size(EntityDimensions.fixed(0.9F, 1.4F)).build());
 	
 	private static <T extends Entity> EntityType<T> register(String name, EntityType<T> builder) {
 		return Registry.register(Registry.ENTITY_TYPE, new Identifier(Mooblooms.MOD_ID, name), builder);
