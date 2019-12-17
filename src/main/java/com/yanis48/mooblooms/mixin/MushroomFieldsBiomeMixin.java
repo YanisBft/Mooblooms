14c27c2968fb42769f24b1aa9e4a4824bb8dd51e
Yanis48 <doublecraft.official@gmail.com>
2019-12-17 19:17:32 +0100

    added cluckshroom spawn

---
+++ src/main/java/com/yanis48/mooblooms/mixin/MushroomFieldsBiomeMixin.java
@@ -0,0 +1,25 @@
+package com.yanis48.mooblooms.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.yanis48.mooblooms.init.MoobloomsEntities;
+
+import net.minecraft.entity.EntityCategory;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.MushroomFieldsBiome;
+
+@Mixin(MushroomFieldsBiome.class)
+public class MushroomFieldsBiomeMixin extends Biome {
+
+	protected MushroomFieldsBiomeMixin(Settings settings) {
+		super(settings);
+	}
+	
+	@Inject(at = @At("RETURN"), method = "<init>()V")
+	private void init(CallbackInfo info) {
+		this.addSpawn(EntityCategory.CREATURE, new Biome.SpawnEntry(MoobloomsEntities.CLUCKSHROOM, 10, 4, 8));
+	}
+}
