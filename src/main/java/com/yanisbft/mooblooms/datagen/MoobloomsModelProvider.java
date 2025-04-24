7a9c41e1b33e6a7904794cc8ef19964e54aaaf77
YanisBft <yanis.briffaut@gmail.com>
2025-04-24 21:51:49 +0200

    fix spawn egg models

--- src/main/java/com/yanisbft/mooblooms/datagen/MoobloomsModelProvider.java
+++ src/main/java/com/yanisbft/mooblooms/datagen/MoobloomsModelProvider.java
@@ -1,13 +1,11 @@
 package com.yanisbft.mooblooms.datagen;
 
+import com.yanisbft.mooblooms.Mooblooms;
 import com.yanisbft.mooblooms.api.Cluckshroom;
 import com.yanisbft.mooblooms.api.Moobloom;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.minecraft.client.data.BlockStateModelGenerator;
-import net.minecraft.client.data.ItemModelGenerator;
-import net.minecraft.client.data.ItemModels;
-import net.minecraft.client.data.Model;
+import net.minecraft.client.data.*;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 
@@ -26,6 +24,8 @@ public class MoobloomsModelProvider extends FabricModelProvider {
 
     @Override
     public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        generateSpawnEggTemplate(itemModelGenerator);
+
         for (Moobloom moobloom : Moobloom.MOOBLOOM_BY_TYPE.values()) {
             registerSpawnEgg(itemModelGenerator, moobloom.getSpawnEgg(), moobloom.getPrimarySpawnEggColor(), moobloom.getSecondarySpawnEggColor());
         }
@@ -35,8 +35,16 @@ public class MoobloomsModelProvider extends FabricModelProvider {
         }
     }
 
+    private static void generateSpawnEggTemplate(ItemModelGenerator itemModelGenerator) {
+        Models.GENERATED_TWO_LAYERS.upload(
+                Mooblooms.id("item/template_spawn_egg"),
+                TextureMap.layered(Mooblooms.id("item/spawn_egg"), Mooblooms.id("item/spawn_egg_overlay")),
+                itemModelGenerator.modelCollector
+        );
+    }
+
     private static void registerSpawnEgg(ItemModelGenerator generator, Item item, int primaryColor, int secondaryColor) {
-        Identifier identifier = generator.upload(item, new Model(Optional.of(Identifier.of("minecraft", "item/template_spawn_egg")), Optional.empty()));
+        Identifier identifier = generator.upload(item, new Model(Optional.of(Mooblooms.id("item/template_spawn_egg")), Optional.empty()));
         generator.output.accept(item, ItemModels.tinted(identifier, ItemModels.constantTintSource(primaryColor), ItemModels.constantTintSource(secondaryColor)));
     }
 }
